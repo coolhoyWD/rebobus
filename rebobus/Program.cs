21c742351f9dadc7be017ee3b0ec61b11d17921d
CoolhoyWD <coolhoy@gmail.com>
2023-01-10 21:47:02 +0500

    Добавьте файлы проекта.

---
+++ rebobus/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
+namespace rebobus
+{ class program { static void Main(string[] args) { for (int i =1000; i > 0; i -= 7) { Console.WriteLine(i + "-" + 7 + "=" + (i - 7));
+                Thread.Sleep(50);
+            } } } }
